5689df309be7b26432d9123f609c942520991efe
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-09-11 18:12:43 -0300

    Create desafio_for.js
    Estrutura de repetição for
    
    Co-Authored-By: Geraldo Luiz <113297903+geeh17@users.noreply.github.com>

---
+++ Estruturas de Controle/desafio_for.js
@@ -0,0 +1,19 @@
+//#
+//##
+//###
+//####
+//#####
+//######
+
+/*
+let linha = '';
+for(let i = 1; i <= 6; i++){
+    linha += '#';
+    console.log(linha);
+}
+*/
+// fazendo coom string sem usar forach
+
+for(let s = '#'; s != '######'; s += '#'){
+    console.log(s);
+}
