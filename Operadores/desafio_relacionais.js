8b912952b7691f8f02d09bb18e8df20de694b4ea
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-09-11 12:20:15 -0300

    Create desafio_relacionais.js
    Desafio relacionais
    
    Co-Authored-By: Geraldo Luiz <113297903+geeh17@users.noreply.github.com>

---
+++ Operadores/desafio_relacionais.js
@@ -0,0 +1,18 @@
+let a = 1;
+let b = 2;
+
+// encontre 5 expressoes que o resultado é TRUE
+
+console.log(a < b);
+console.log(a != b);
+console.log(a <= b);
+console.log(b === b);
+console.log(a < b);
+
+// encontre 5 expressoes que o resultado é FALSE
+
+console.log(a == b);
+console.log(a >= b);
+console.log(a === b);
+console.log(b === a);
+console.log(a > b);
