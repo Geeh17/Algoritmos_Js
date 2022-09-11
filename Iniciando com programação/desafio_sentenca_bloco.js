b5a045569aaa10c77360130b366a3744f4bdd836
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-09-11 12:18:51 -0300

    Create desafio_sentenca_bloco.js
    desafio sentença de blocos
    
    Co-Authored-By: Geraldo Luiz <113297903+geeh17@users.noreply.github.com>

---
+++ Iniciando com programação/desafio_sentenca_bloco.js
@@ -0,0 +1,18 @@
+//Dividir em dois blocos numeros pares e numeros impares.
+{
+    console.log("Bloco 1 - números ímpares")
+    console.log("1")
+    console.log("3")
+    console.log("5")
+    console.log("7")
+    console.log("9")
+}
+
+{
+    console.log("Bloco 2 - números pares")
+    console.log("2")
+    console.log("4")
+    console.log("6")
+    console.log("8")
+    console.log("10")
+}
