ed05df1ea8d4b80a4dbd4e94d4693745002c3b4b
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-09-12 17:44:16 -0300

    Create desafio_funcao.js
    Desafio de função.
    
    Co-Authored-By: Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>

---
+++ Funçoes/desafio_funcao.js
@@ -0,0 +1,25 @@
+// soma, subtração, multiplicação e divição
+
+function soma (num1, num2){
+    console.log(num1 + num2);
+}
+soma(3,4);
+soma(4,4);
+
+function subtracao (num1, num2){
+    console.log(num1 - num2);
+}
+subtracao(15,6);
+subtracao(5,6);
+
+function multiplicacao (num1, num2){
+    console.log(num1 * num2);
+}
+multiplicacao(2,6);
+multiplicacao(5,6);
+
+function divicao (num1, num2){
+    console.log(num1 / num2);
+}
+divicao(6,6);
+divicao(36,6);
